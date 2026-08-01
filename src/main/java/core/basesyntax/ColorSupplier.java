a02584a8ced0ccbf338b60052013bdcbe548a6a0
goffa-exe <n4v4rm0re666@gmail.com>
2026-08-02 00:01:07 +0200

    add .name() for enum instead of toString()

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -7,6 +7,6 @@ public class ColorSupplier {
         int randomColorNumber = new Random().nextInt(Color.values().length);
         Color color = Color.values()[randomColorNumber];
 
-        return color.toString();
+        return color.name();
     }
 }
