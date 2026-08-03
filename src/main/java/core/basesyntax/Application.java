8f1c02fcbb8bb7f90c1f4e03c018cba4c2bf2bee
goffa-exe <n4v4rm0re666@gmail.com>
2026-08-03 20:36:42 +0200

    add magicNumber, removed toString() from Application

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,9 +3,10 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
+        final int magicNumber = 3;
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println(lottery.getRandomBall().toString());
+        for (int i = 0; i < magicNumber; i++) {
+            System.out.println(lottery.getRandomBall());
         }
     }
 }
